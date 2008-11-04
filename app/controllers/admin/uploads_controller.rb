26696ac464cf01bea843d3dac5e49bb0233be2ff
Andreas Zuber <github@puzzle.ch>
2008-11-04 19:20:22 +0100

    After the upload, the files url is now shown without displaying the id. It is now not possible for a user to browse the files with the id. Admin can delete the files now.

--- app/controllers/admin/uploads_controller.rb
+++ app/controllers/admin/uploads_controller.rb
@@ -7,14 +7,66 @@ class Admin::UploadsController < ApplicationController
     end
   end
 
-# GET /uploads
-  # GET /uploads.xml
+
+  # GET /admin/uploads/1
+  # GET /admin/uploads/1.xml
+  def show
+    @upload = Upload.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @upload }
+    end
+  end
+
+  # GET /admin/uploads/1/edit
+  def edit
+    @upload = Upload.find(params[:id])
+  end
+
+  # GET /admin/uploads
+  # GET /admin/uploads.xml
   def index
     @uploads = Upload.find(:all)
 
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @uploads }
+    end
+  end
+
+  # PUT /admin/uploads/1
+  # PUT /admin/uploads/1.xml
+  def update
+    @upload = Upload.find(params[:id])
+   
+    respond_to do |format|
+      if @upload.update_attributes(params[:upload])
+        flash[:notice] = 'Upload was successfully updated.'
+        format.html { redirect_to([:admin, @upload]) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @upload.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /admin/uploads/1
+  # DELETE /admin/uploads/1.xml
+  def destroy
+    @upload = Upload.find(params[:id])
+
+    dirname = File.join RAILS_ROOT, 'public', 'downloads', @upload.hash_key
+    filepath = File.join dirname, @upload.filename
+    
+    File.delete filepath
+    Dir.delete dirname
+    @upload.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(admin_uploads_url) }
+      format.xml  { head :ok }
     end
   end
 
--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,15 +1,5 @@
 class UploadsController < ApplicationController
 
-  # GET /uploads/1
-  # GET /uploads/1.xml
-  def show
-    @upload = Upload.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @upload }
-    end
-  end
 
   # GET /uploads/new
   # GET /uploads/new.xml
@@ -22,11 +12,6 @@ class UploadsController < ApplicationController
     end
   end
 
-  # GET /uploads/1/edit
-  def edit
-    @upload = Upload.find(params[:id])
-  end
-
   # POST /uploads
   # POST /uploads.xml
   def create
@@ -34,12 +19,12 @@ class UploadsController < ApplicationController
    
     # Create a new hash
     chars = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
-    random_hash = ""
+    hash_key = ""
     @filepaste_settings['general']['hash_length'].to_i.times do
-      random_hash << chars[rand(chars.size)]
+      hash_key << chars[rand(chars.size)]
     end
 
-    dirname = File.join RAILS_ROOT, 'public', 'downloads', random_hash
+    dirname = File.join RAILS_ROOT, 'public', 'downloads', hash_key
     filepath = File.join dirname, datafile.original_filename
 
     # Now we save the File to the disk
@@ -50,7 +35,7 @@ class UploadsController < ApplicationController
     @upload = Upload.new
     @upload.filename = datafile.original_filename
     @upload.description = params[:uploads][:description]
-    @upload.hash_key = random_hash
+    @upload.hash_key = hash_key
     @upload.content_type = datafile.content_type
     @upload.updated_at = Time.now
     @upload.created_at = Time.now
@@ -58,7 +43,7 @@ class UploadsController < ApplicationController
     respond_to do |format|
       if @upload.save
         flash[:notice] = 'Upload was successfully created.'
-        format.html { redirect_to(@upload) }
+        format.html { redirect_to( :action => 'downloadinfo', :id => hash_key ) }
         format.xml  { render :xml => @upload, :status => :created, :location => @upload }
       else
         format.html { render :action => "new" }
@@ -67,39 +52,13 @@ class UploadsController < ApplicationController
     end
   end
 
-  # PUT /uploads/1
-  # PUT /uploads/1.xml
-  def update
-    @upload = Upload.find(params[:id])
-   
-    respond_to do |format|
-      if @upload.update_attributes(params[:upload])
-        flash[:notice] = 'Upload was successfully updated.'
-        format.html { redirect_to(@upload) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @upload.errors, :status => :unprocessable_entity }
+  def downloadinfo
+    @upload = Upload.find(:first, :conditions => [ 'hash_key = ?', params[:id] ])
+    flash[:notice] = 'No such file.'
+    if @upload.nil?
+      respond_to do |format|
+        format.html { render :action => "new" }
       end
     end
   end
-
-  # DELETE /uploads/1
-  # DELETE /uploads/1.xml
-  def destroy
-    @upload = Upload.find(params[:id])
-
-    dirname = File.join RAILS_ROOT, 'public', 'downloads', @upload.random_hash
-    filepath = File.join dirname, @upload.filename
-    
-    File.delete filepath
-    Dir.delete dirname
-    @upload.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(uploads_url) }
-      format.xml  { head :ok }
-    end
-  end
-
 end
