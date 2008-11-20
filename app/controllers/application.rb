94abbc3c4b80d852030bc16fa1cdd1084b9d70ef
Andreas Zuber <github@puzzle.ch>
2008-11-20 17:05:03 +0100

    Licence header added to modified files

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,5 +1,18 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
+# Copyright (c) 2008 Andreas Zuber. All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 require 'net/ldap'
 
 class ApplicationController < ActionController::Base
@@ -10,11 +23,6 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '87756b121176328abb8c413a7674af31'
-  
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
   
   def load_settings
     @filepaste_settings = YAML.load( File.open( RAILS_ROOT + "/config/settings.yml" ) )
